e9d307f297ed3e02b109e571f752e045b80bdd1d
Aaron Amberman <aaronamberman@gmail.com>
2023-10-04 14:09:21 -0400

    Initial check in of project

---
+++ LTT-WPF-Application/LTT-WPF-Application/Services/ThemingService.cs
@@ -0,0 +1,42 @@
+﻿using LTT_WPF_Application.Theming;
+using System.Windows;
+
+namespace LTT_WPF_Application.Services
+{
+    internal class ThemingService
+    {
+        #region Fields
+
+        private Theme theme;
+
+        #endregion
+
+        #region Properties
+
+        public Theme Theme 
+        { 
+            get => theme;
+            set 
+            {
+                theme = value;
+
+                ChangeTheme(value);
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void ChangeTheme(Theme theme)
+        {
+            foreach (ResourceDictionary rd in Application.Current.Resources.MergedDictionaries)
+            {
+                if (rd is ThemeDictionary td)
+                    td.UpdateTheme(theme);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ LTT-WPF-Application/LTT-WPF-Application/Theming/Theme.cs
@@ -0,0 +1,8 @@
+﻿namespace LTT_WPF_Application.Theming
+{
+    internal enum Theme
+    {
+        Light = 0,
+        Dark = 1
+    }
+}
